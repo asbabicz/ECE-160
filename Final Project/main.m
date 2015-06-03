c2e2f4874c3f0500c65158377868a2fabc93fe0f
asbabicz <asbabicz@gmail.com>
2015-06-03 02:30:13 -0700

    Big update, see description for details
    1. main.m - Added encoding and decoding for the I frames. I frames are
    now JPEG compressed. Also implemented the caption code into a single
    loop for more efficiency and make it dynamic
    
    2. Added Iencode.m - Encoder for the I frames
    
    3. Added Idecode.m - Decoder for the I frames
    
    4. Added forwardDCT.m - DCT function; needed for Iencoder
    
    5. Added Qtable2.txt - Quantization table; needed for Iencoder and
    Idecoder

--- Final Project/main.m
+++ Final Project/main.m
@@ -9,9 +9,9 @@ clc
 %   this folder, you should have the 30 frames we want to process. The
 %   frames must be named "FRAME000.png", "FRAME001.png", ... "FRAME030.png"
 %
-% For the purposes of speed, I've re-sized each of the PNG files during
-%   the import process. Otherwise this code would take significant time to
-%   run (on a fast desktop, ~5 min).
+% For the purposes of speed, I've re-sized each of the PNG files during the
+%   import process. Otherwise this code would take significant time to run
+%   (on a fast desktop, ~5 min).
 % -------------------------------------------------------------------------
 
 % -------------------------------------------------------------------------
@@ -22,41 +22,46 @@ clc
 %   ^1     ^5     ^9     ^13    ^17    ^21    ^25    ^29
 % -------------------------------------------------------------------------
 
-string = 'High5_PNG/FRAME0';
-oldFrames{30} = []; % Container for the original frames
-newFrames{30} = []; % Contianer for the new frames
+string             = 'High5_PNG/FRAME0';
+originalFrames{30} = []; % Container for the original frames
+finalFrames{30}    = []; % Container for the final frames
 
 for x = 1 : 30
 
     if x < 10
         newString = strcat( string, '0', num2str(x) ); % Add leading zero
-    else
+	else
         newString = strcat( string, num2str(x) );
     end
 
     % Import each frames
-	oldFrames{x} = double( imresize( rgb2gray( imread( strcat(newString,...
-                                                '.png') ) ), [256 384]) );
+	originalFrames{x} = double( imresize( rgb2gray( imread( ...
+                               strcat(newString, '.png') ) ), [256 384]) );
 
 end
 
 % -------------------------------------------------------------------------
-% Copy over only the I frames to container containing the final frames
+% Encode the I frames and store them into the container of final frames
 % -------------------------------------------------------------------------
 for x = 1 : 4 : 30
 
-	newFrames{x} = oldFrames{x};
+    % Encode
+    [code, dict, dim] = Iencode( originalFrames{x} );
+    
+    % Decode and store
+    finalFrames{x}    = Idecode(code, dict, dim);
 
 end
 
-% Manually import and copy over the last frame
-newFrames{30} = oldFrames{30};
-
+% Manually encode the last I frame and store it
+[code, dict, dim] = Iencode( originalFrames{30} );
+finalFrames{30}   = Idecode(code, dict, dim);
+    
 MBsize  = 16; % Macroblock size
 p       = 2;  % Search parameter (the larger, the longer the runtime)
 
 % -------------------------------------------------------------------------
-% Calculate the P frames. This is done using these steps:
+% JPEG compress the P frames. This is done using these steps:
 %   1. Find motion vectors between I -> P
 %   2. Find the motion compensated P frame using the motion vectors
 %   3. Calculate the error between the motion compensated P frame and the
@@ -66,18 +71,20 @@ p       = 2;  % Search parameter (the larger, the longer the runtime)
 % -------------------------------------------------------------------------
 for y = 3 : 4 : 30
 
-    I = oldFrames{y - 2};
-    P = oldFrames{y};
+    I = originalFrames{y - 2};
+    P = originalFrames{y};
 
     % Motion vector between I -> P
     motionVectIP  = motionEstES(I, P, MBsize, p);
+    
     % Compensated image between I -> P
     P_comp = motionComp(I, motionVectIP, MBsize);
+    
     % Error between reconstructed image and original P frame
     P_error = P - P_comp;
 
-    newFrames{y} = I - P_error;
-
+    finalFrames{y} = I - P_error;
+    
 end
 
 % -------------------------------------------------------------------------
@@ -94,9 +101,9 @@ end
 % -------------------------------------------------------------------------
 for z = 2 : 4 : 28
 
-    I   = oldFrames{z - 1}; % This I is behind the B frame
-    B   = oldFrames{z};
-    P   = oldFrames{z + 1}; % This P is in front of the B frame
+    I   = originalFrames{z - 1}; % This I is behind the B frame
+    B   = originalFrames{z};
+    P   = originalFrames{z + 1}; % This P is in front of the B frame
 
     % Motion vectors between I -> B && P -> B
     motionVectIB = motionEstES(I, B, MBsize, p);
@@ -112,7 +119,7 @@ for z = 2 : 4 : 28
     % Error between reconstructed image and original B frame 
     Bframe_error = B - imgCompBavg;
 
-    newFrames{z} = Bframe_error;
+    finalFrames{z} = Bframe_error;
 
 end
 
@@ -130,9 +137,9 @@ end
 % -------------------------------------------------------------------------
 for z = 4 : 4 : 28
 
-    I   = oldFrames{z + 1}; % This I is in front of the B frame
-    B   = oldFrames{z};
-    P   = oldFrames{z - 1}; % This P is behind the B frame
+    I   = originalFrames{z + 1}; % This I is in front of the B frame
+    B   = originalFrames{z};
+    P   = originalFrames{z - 1}; % This P is behind the B frame
 
     % Motion vectors between I -> B && P -> B
     motionVectIB = motionEstES(I, B, MBsize, p) ;
@@ -148,58 +155,33 @@ for z = 4 : 4 : 28
     % Error between reconstructed image and original B frame 
     Bframe_error = B - imgCompBavg;
 
-    newFrames{z} = Bframe_error;
+    finalFrames{z} = Bframe_error;
 
 end
+
+% -------------------------------------------------------------------------
+% Add captions to the appropriate frames
+% -------------------------------------------------------------------------
 
 % Import captions from .csv file
-captions = readtable('High5_PNG\caption.csv');
-size = size( oldFrames{1} );
-height = size(1,1);
-width  = size(1,2);
+captions = readtable('High5_PNG/caption.csv');
+size = size( originalFrames{1} );
+imgHeight = size(1,1);
+imgWidth  = size(1,2);
 
-% -------------------------------------------------------------------------
-% Add FIRST captions to the appropriate frames
-% -------------------------------------------------------------------------
-Text = char( captions{1,3} );
-H = vision.TextInserter(Text);
-H.Color = [255 255 255];
-H.FontSize = 20; % CHANGE TO 60 FOR FINAL CODE
-H.Location = [ (width / 5) (5 * height / 6)]; % CHANGE TO []
-
-for x = captions{1,1} : ( captions{1,1} + captions{1,2} )
+for x = 1 : height(captions)
     
-	newFrames{x} = step(H, newFrames{x});
+    Text = char( captions{x,3} );
+    H = vision.TextInserter(Text);
+    H.Color = [255 255 255];
+    H.FontSize = 20; % CHANGE TO 60 FOR FINAL CODE
+    H.Location = [ (imgWidth / 5) (5 * imgHeight / 6)]; % CHANGE TO [__ __]
     
-end
-
-% -------------------------------------------------------------------------
-% Add SECOND captions to the appropriate frames
-% -------------------------------------------------------------------------
-Text = char( captions{2,3} );
-H = vision.TextInserter(Text);
-H.Color = [255 255 255];
-H.FontSize = 20;
-H.Location = [ (width / 5) (5 * height / 6)];
-
-for x = captions{2,1} : ( captions{2,1} + captions{2,2} )
-    
-	newFrames{x} = step(H, newFrames{x});
-    
-end
-
-% -------------------------------------------------------------------------
-% Add THIRD captions to the appropriate frames
-% -------------------------------------------------------------------------
-Text = char( captions{3,3} );
-H = vision.TextInserter(Text);
-H.Color = [255 255 255];
-H.FontSize = 20;
-H.Location = [ (width / 5) (5 * height / 6)];
-
-for x = captions{3,1} : ( captions{3,1} + captions{3,2} )
-    
-    newFrames{x} = step(H, newFrames{x});
+	for y = captions{x,1} : ( captions{x,1} + captions{x,2} )
+      
+        finalFrames{y} = step(H, finalFrames{y});
+       
+	end
     
 end
 
@@ -210,7 +192,7 @@ end
 % -------------------------------------------------------------------------
 for a = 1 : 30
 
-    imwrite( uint8( newFrames{a} ), strcat( 'High5_JPG/frame', ...
+    imwrite( uint8( finalFrames{a} ), strcat( 'High5_JPG/frame', ...
                                                     num2str(a), '.jpg' ) );
 
 end
